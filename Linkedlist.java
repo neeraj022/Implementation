caddc457b9f77ed27d31797dc7de69f1bf179698
Neeraj Yadav <neerajyadav022@gmail.com>
2017-02-13 15:37:25 +0530

    method added to add two linked list

--- Linkedlist.java
+++ Linkedlist.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Linkedlist
 {
 	public static void main(String args[])
@@ -5,11 +6,24 @@ public class Linkedlist
 		Node head=new Node(35);
 		head.append(24);
 		head.append(26);
+		head.append(24);
 		head.append(50);
 		head.append(45);
+		//head.printLinkedList(head);
+		//head.deleteNode(head, 26);
+		head.append(50);
 		head.printLinkedList(head);
-		head.deleteNode(head, 26);
+		head.deleteDuplicateWoBuffer(head);
 		head.printLinkedList(head);
+
+		Node add1=new Node(3);
+		add1.append(1);
+		add1.append(5);
+		add1.append(1);
+		Node add2=new Node(5);
+		add2.append(9);
+		add2.append(2);
+		add1.addTwoNumbers(add1,add2);
 	}
 }
 
@@ -92,6 +106,55 @@ class Node
 		}
 	}
 
+	public void deleteDuplicate(Node head)
+	{
+		Node previous=null;
+		Node iterator=head;
+		Hashtable table=new Hashtable();
+		while(iterator!=null)
+		{
+			if(table.containsKey(iterator.data))
+			{
+				previous.next=iterator.next;
+			}
+			else
+			{
+				table.put(iterator.data, true);
+				previous=iterator;
+			}
+			iterator=iterator.next;
+		}
+	}
+
+	public void deleteDuplicateWoBuffer(Node head)
+	{
+		Node iterator1=head;
+		Node iterator2=head;
+		Node previous=head;
+		while(iterator1!=null)
+		{
+			boolean isDuplicate=false;
+			while(iterator2!=iterator1)
+			{
+				if(iterator2.data==iterator1.data)
+				{
+					isDuplicate=true;
+				}
+				iterator2=iterator2.next;
+			}
+			if(isDuplicate)
+			{
+				previous.next=iterator1.next;
+			}
+			else
+			{
+				previous=iterator1;
+			}
+			iterator2=head;
+			iterator1=iterator1.next;
+		}
+	}
+
 	public void printLinkedList(Node head)
 	{
 		Node node=head;
@@ -102,5 +165,34 @@ class Node
 		}
 		System.out.print(node.data);
 		System.out.println("");
+	}
+
+	/*
+	This is custom method used to sum two numbers represented by linked lists
+	example:
+	input: (3->1->5)+(5->9->2)
+	output: (8->0->8) where head is units digit
+	*/
+	public void addTwoNumbers(Node head1, Node head2)
+	{
+		Node result=new Node();
+		Node resultIterator=result;
+		int quotient=0;
+		while(head1!=null || head2!=null)
+		{
+			int temp=(head1==null? 0 : head1.data)+(head2==null? 0 : head2.data)+quotient;
+			resultIterator.append(temp%10);
+			quotient=temp/10;
+			if(head1!=null)
+			{
+				head1=head1.next;
+			}
+			if(head2!=null)
+			{
+				head2=head2.next;
+			}
+		}
+		result=result.next;
+		result.printLinkedList(result);
 	}
 }
--- README.md
+++ README.md
@@ -3,4 +3,6 @@ This repository will have various implementation of Data structures using arrays
 Enumerating the implementations:<br>
 1) Stack using arrays <br>
 2) Queue using arrays <br>
-3) Linked List implementation
+3) Linked List implementation<br>
+	3.1) Add two number represented by two linked list. Head point to 1's digit
+
